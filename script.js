592b39370970f76bb617bc56a9df3f945c579d25
gemeozil10-byte <gemeozil10@gmail.com>
2026-07-23 00:27:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("app").innerHTML = `
+<h1 style="text-align:center;color:#6b4f3a;">
+☕ Omar Khayyam Cafe
+</h1>
+
+<p style="text-align:center;">
+Digital Menu Coming Soon
+</p>
+`;
