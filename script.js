24651adc1242fd08d7a7af116092a74eb53cbe2d
gemeozil10-byte <gemeozil10@gmail.com>
2026-07-23 22:16:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.getElementById("app").innerHTML = `
-<h1 style="text-align:center;color:#6b4f3a;">
-☕ Omar Khayyam Cafe
-</h1>
-
-<p style="text-align:center;">
-Digital Menu Coming Soon
-</p>
-`;
+// سيتم إضافة وظائف الموقع هنا لاحقًا.
