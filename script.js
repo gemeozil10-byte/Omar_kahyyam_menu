919c5c7d726f83c2aeeb1522be4708681586390d
gemeozil10-byte <gemeozil10@gmail.com>
2026-07-24 03:26:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// سيتم إضافة وظائف الموقع هنا لاحقًا.
+const searchInput = document.getElementById("searchInput");
+const menuItems = document.querySelectorAll(".menu-item");
+
+if (searchInput) {
+    searchInput.addEventListener("input", function () {
+        const value = this.value.toLowerCase();
+
+        menuItems.forEach(item => {
+            const text = item.textContent.toLowerCase();
+
+            if (text.includes(value)) {
+                item.style.display = "flex";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    });
+}
